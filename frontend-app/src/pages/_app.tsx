8af748c3b09a7348464ac7096e799acf77997381
xeusteerapat <xeus085@gmail.com>
2022-10-07 12:10:54 +0700

    :lock: [Update] finish nextauth setup

--- frontend-app/src/pages/_app.tsx
+++ frontend-app/src/pages/_app.tsx
@@ -1,7 +1,15 @@
+import { SessionProvider, SessionProviderProps } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+function MyApp({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps<SessionProviderProps>) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  );
 }
 
 export default MyApp;
--- frontend-app/src/pages/index.tsx
+++ frontend-app/src/pages/index.tsx
@@ -1,9 +1,20 @@
 import type { NextPage } from 'next';
+import { signIn, signOut, useSession } from 'next-auth/react';
 
 const Home: NextPage = () => {
+  const { data, status } = useSession();
+
   return (
     <div>
       <h1>Hi</h1>
+      {!data?.user ? (
+        <button onClick={() => signIn('google')}>Sign in</button>
+      ) : (
+        <div>
+          <h2>{data.user.name}</h2>
+          <button onClick={() => signOut()}>Sign Out</button>
+        </div>
+      )}
     </div>
   );
 };
