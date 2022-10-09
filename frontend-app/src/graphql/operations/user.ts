08a8e1c8a0359e3b343e889c2cc3108bca336e23
xeusteerapat <xeus085@gmail.com>
2022-10-09 13:52:40 +0700

    :sparkles: [Update] update search query

--- frontend-app/src/graphql/operations/user.ts
+++ frontend-app/src/graphql/operations/user.ts
@@ -1,7 +1,16 @@
 import { gql } from '@apollo/client';
 
 export const userOperations = {
-  Queries: {},
+  Queries: {
+    searchUsers: gql`
+      query SearchedUser($username: String!) {
+        searchUsers(username: $username) {
+          id
+          username
+        }
+      }
+    `,
+  },
   Mutations: {
     createUsername: gql`
       mutation CreateUsername($username: String!) {
