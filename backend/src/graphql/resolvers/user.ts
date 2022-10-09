8c61591a84ae870321289ef77713ac5536fa0bf3
xeusteerapat <xeus085@gmail.com>
2022-10-09 09:12:49 +0700

    :bug: [Fix] fix types error

--- backend/src/graphql/resolvers/user.ts
+++ backend/src/graphql/resolvers/user.ts
@@ -1,12 +1,60 @@
+import { CreateUsernameResponse } from './../../types/interfaces/user-response';
+import { GraphQLContext } from './../../types/interfaces/graphql-context';
+
 export const userResolvers = {
   Query: {
     searchUsers: () => {},
   },
   Mutation: {
-    createUsername: (_: any, args: { username: string }, context: any) => {
+    createUsername: async (
+      _: any,
+      args: { username: string },
+      context: GraphQLContext
+    ): Promise<CreateUsernameResponse | undefined> => {
       const { username } = args;
-      console.log('create username', username);
-      console.log('create ctx', context);
+      const { session, prisma } = context;
+
+      if (!session?.user) {
+        return {
+          error: 'Not Authenticated',
+        };
+      }
+
+      const userId = session.user.id;
+
+      try {
+        const existsUser = await prisma.user.findUnique({
+          where: {
+            username,
+          },
+        });
+
+        if (existsUser) {
+          return {
+            error: 'Username already taken',
+          };
+        }
+
+        await prisma.user.update({
+          where: {
+            id: userId,
+          },
+          data: {
+            username,
+          },
+        });
+
+        return {
+          success: true,
+        };
+      } catch (error) {
+        if (error instanceof Error) {
+          console.log('create username error:', error);
+          return {
+            error: error?.message,
+          };
+        }
+      }
     },
   },
   // Subscription: {
